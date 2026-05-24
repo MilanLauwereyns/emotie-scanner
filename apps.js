63fe040de670a49f217470c1a65c60aee2bc3290
MilanLauwereyns <135699016+MilanLauwereyns@users.noreply.github.com>
2026-05-24 13:23:22 +0200

    feature: teachable machine added

--- apps.js
+++ apps.js
@@ -1,4 +1,7 @@
 
+const URL = "./model/";
+
+let model;
 let webcam;
 
 async function startScanner() {
@@ -16,6 +19,11 @@ async function startScanner() {
 
   try {
 
+    model = await tmImage.load(
+      URL + "model.json",
+      URL + "metadata.json"
+    );
+
     const stream =
       await navigator.mediaDevices.getUserMedia({
         video: true,
@@ -27,6 +35,8 @@ async function startScanner() {
     webcam.onloadedmetadata = () => {
 
       webcam.play();
+
+      loop();
     };
 
   } catch (err) {
@@ -37,4 +47,113 @@ async function startScanner() {
       "Camera werkt niet"
     );
   }
+}
+
+async function loop() {
+
+  try {
+
+    const prediction =
+      await model.predict(webcam);
+
+    let highest =
+      prediction[0];
+
+    prediction.forEach((p) => {
+
+      if (
+        p.probability >
+        highest.probability
+      ) {
+
+        highest = p;
+      }
+    });
+
+    showMood(
+      highest.className,
+      highest.probability
+    );
+
+  } catch (err) {
+
+    console.error(err);
+  }
+
+  requestAnimationFrame(loop);
+}
+
+function showMood(
+  mood,
+  confidence
+) {
+
+  let emoji = "🙂";
+  let message = "";
+
+  switch (
+    mood.toLowerCase()
+  ) {
+
+    case "blij":
+
+      emoji = "😎";
+
+      message =
+        "Je bent blij vandaag";
+
+      break;
+
+    case "moe":
+
+      emoji = "😴";
+
+      message =
+        "Je lijkt moe";
+
+      break;
+
+    case "gestrest":
+
+      emoji = "😵";
+
+      message =
+        "Je lijkt gestrest";
+
+      break;
+
+    case "verdrietig":
+
+      emoji = "🥺";
+
+      message =
+        "Kop op 💛";
+
+      break;
+
+    default:
+
+      emoji = "🙂";
+
+      message =
+        "Mood gevonden";
+  }
+
+  /* UI */
+
+  document
+    .getElementById("emoji")
+    .innerText = emoji;
+
+  document
+    .getElementById("result")
+    .innerText = message;
+
+  document
+    .getElementById("confidence")
+    .innerText =
+      "Confidence: " +
+      Math.round(
+        confidence * 100
+      ) + "%";
 }
