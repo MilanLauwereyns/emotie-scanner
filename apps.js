17f78bb1eaed08311049bc9724895410a38663e7
MilanLauwereyns <135699016+MilanLauwereyns@users.noreply.github.com>
2026-05-24 12:54:04 +0200

    feature webcam: added webcam to htlm and javascript

--- apps.js
+++ apps.js
@@ -1,4 +1,7 @@
-function startScanner() {
+
+let webcam;
+
+async function startScanner() {
 
   document
     .getElementById("startScreen")
@@ -7,4 +10,31 @@ function startScanner() {
   document
     .getElementById("scanner")
     .classList.remove("hidden");
+
+  webcam =
+    document.getElementById("webcam");
+
+  try {
+
+    const stream =
+      await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false
+      });
+
+    webcam.srcObject = stream;
+
+    webcam.onloadedmetadata = () => {
+
+      webcam.play();
+    };
+
+  } catch (err) {
+
+    console.error(err);
+
+    alert(
+      "Camera werkt niet"
+    );
+  }
 }
